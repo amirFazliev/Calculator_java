7dae2c66a9ba7caedd869d5633ec7a502d03ebc0
Amir Fazliev <fazliev.amir@yandex.ru>
2022-11-06 19:43:14 +0300

    change java file

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+package hm41Lyambda;
+
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    //BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? -1 : x / y; // не было проверки на знаменатель (делить на 0 нельзя)
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println =System.out::println;
+}
